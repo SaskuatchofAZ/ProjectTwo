f67dadff30df8349e64e0f23aa18bc6f099ef4af
FCTESLA <fcamacho888@gmail.com>
2020-05-15 14:43:36 -0700

    messing with the catController

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 // Import our controllers
 const postRoutes = require("./postsController");
 const userRoutes = require("./usersController");
+const animalsRoutes = require("./animalsController");
+const catRoutes = require("./catController");
 
 // Hook up to the router
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
+router.use("/allAnimals", animalsRoutes);
+router.use("/cats", catRoutes);
 
 // Export the router
 module.exports = router;
