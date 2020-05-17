e2a5a038463cd9761866ed4ead75d50a6665571b
FCTESLA <fcamacho888@gmail.com>
2020-05-16 19:51:32 -0700

    creating tables cats.handlebars

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -8,21 +8,21 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 /**
  * Home Page
  */
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("index", { user: req.user });
 });
 
 /**
  * Home Page, again
  */
-router.get("/home", function(req, res) {
+router.get("/home", function (req, res) {
   res.render("index", { user: req.user });
 });
 
 /**
  * Signup page
  */
-router.get("/signup", function(req, res) {
+router.get("/signup", function (req, res) {
   if (req.user) {
     res.redirect("/");
   } else {
@@ -33,7 +33,7 @@ router.get("/signup", function(req, res) {
 /**
  * Login page
  */
-router.get("/login", function(req, res) {
+router.get("/login", function (req, res) {
   if (req.user) {
     res.redirect("/");
   } else {
@@ -45,20 +45,27 @@ router.get("/login", function(req, res) {
  * Forum Page -
  * Notice loading our posts, with that include!
  */
-router.get("/forum", isAuthenticated, function(req, res) {
+router.get("/forum", isAuthenticated, function (req, res) {
   db.Post.findAll({ raw: true, include: [db.User] }) // Joins User to Posts! And scrapes all the seqeulize stuff off
     .then(dbModel => {
       res.render("forum", { user: req.user, posts: dbModel });
     })
     .catch(err => res.status(422).json(err));
+
 });
 /**
  * All Animals
  */
-router.get("/allAnimals", function(req, res) {
-  db.Animal.findAll({ raw: true, include: [db.User] }).then(dbModel => {
+router.get("/allAnimals", function (req, res) {
+  db.Animal.findAll({
+    raw: true,
+    include: [db.User]
+  }).then(dbModel => {
     console.log(dbModel);
-    res.render("allAnimals", { user: req.user, animal: dbModel });
+    res.render("allAnimals", {
+      user: req.user,
+      animal: dbModel
+    });
   });
 });
 
@@ -66,7 +73,13 @@ router.get("/allAnimals", function(req, res) {
  * Cats
  */
 router.get("/cats", function(req, res) {
-  res.render("cats", { user: req.user });
+  db.Animal.findAll({
+    where: {
+      species: "cat"
+    }
+  }).then(dbModel => {
+    res.render("cats", { animal: dbModel });
+  });
 });
 
 /**
